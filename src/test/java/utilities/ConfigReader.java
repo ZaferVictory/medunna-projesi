fa5a83c3f5461f66cd8bf0ca8112cfa36a441851
ZAFER <zaferkosktepe@gmail.com>
2023-01-30 19:29:45 +0300

    30.01.2023

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,36 +1,34 @@
 package utilities;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-    // 1- Properties objesi olusturacagiz
+    public static Properties properties;
 
-    static Properties properties;
-
-    // 2- Bu class'in amaci configuration.properties dosyasini okumak
-    //    ve oraadaki key value ikililerini kullanarak istedigimiz key'e ait value'yu bize getirmek
-    //
-    static {
+    static{
         String dosyaYolu="configuration.properties";
+
         try {
-            FileInputStream fileInputStream=new FileInputStream(dosyaYolu);
+            FileInputStream fis=new FileInputStream(dosyaYolu);
+            // fis dosyayolunu tanimladigimiz configuration.properties dosyasini okudu
             properties=new Properties();
-            properties.load(fileInputStream);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            properties.load(fis); // fis'in okudugu bilgileri properties'e yukledi
+            fis.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    // 3- test class'larindan configReader class'ina ulasip yukaridaki islemleri
-    //    yapmamizi saglayacak bir method olusturacagiz
     public static String getProperty(String key){
+        /*
+          test method'undan yolladigimiz string key degerini alip
+          Properties class'indan getProperty( ) method'unu kullanarak
+          bu key'e ait value'u bize getirdi
+         */
 
-        String value=properties.getProperty(key);
-        return value;
+        return properties.getProperty(key);
     }
 }
